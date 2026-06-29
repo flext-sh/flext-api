4819b07b8fe2ec304c1b630f7f04dcdd63399238
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-06-29 19:58:16 -0300

    Thin generated exports and owner imports

--- src/flext_api/_utilities/__init__.py
+++ src/flext_api/_utilities/__init__.py
@@ -3,30 +3,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
-if TYPE_CHECKING:
-    from flext_api._utilities.api_pydantic import (
-        FlextApiUtilitiesApiPydantic as FlextApiUtilitiesApiPydantic,
-    )
-    from flext_api._utilities.client import FlextApiClient as FlextApiClient
-    from flext_api._utilities.client_codec import (
-        FlextApiClientCodecMixin as FlextApiClientCodecMixin,
-    )
-    from flext_api._utilities.client_request import (
-        FlextApiClientRequestMixin as FlextApiClientRequestMixin,
-    )
-    from flext_api._utilities.request_utils import (
-        FlextApiUtilitiesRequestUtils as FlextApiUtilitiesRequestUtils,
-    )
-    from flext_api._utilities.serializers import (
-        FlextApiUtilitiesSerializers as FlextApiUtilitiesSerializers,
-    )
-    from flext_api._utilities.settings_manager import (
-        FlextApiUtilitiesSettingsManager as FlextApiUtilitiesSettingsManager,
-    )
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".api_pydantic": ("FlextApiUtilitiesApiPydantic",),
