a0381d966dd415652c00f6da5bbb8014077f46c0
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-14 16:46:27 -0300

    fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,9 +10,9 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 if TYPE_CHECKING:
     from tests.constants import TestsFlextApiConstants as TestsFlextApiConstants, c as c
     from tests.typings import TestsFlextApiTypes as TestsFlextApiTypes, t as t
-    from tests.protocols import TestsFlextApiProtocols as TestsFlextApiProtocols, p as p
+    from tests.protocols import TestsFlextApiProtocols as TestsFlextApiProtocols, p
     from tests.models import TestsFlextApiModels as TestsFlextApiModels, m as m
-    from tests.utilities import TestsFlextApiUtilities as TestsFlextApiUtilities, u as u
+    from tests.utilities import TestsFlextApiUtilities as TestsFlextApiUtilities, u
     from tests.base import TestsFlextApiServiceBase as TestsFlextApiServiceBase, s as s
 
 _LAZY_IMPORTS = build_lazy_import_map(
