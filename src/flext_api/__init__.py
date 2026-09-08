ea5127af20a5dc7a499f4368a6279f000d294a48
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 13:02:14 -0300

    fixes

--- src/flext_api/__init__.py
+++ src/flext_api/__init__.py
@@ -22,7 +22,7 @@ from .__version__ import (
 if TYPE_CHECKING:
     from flext_web import d, e, h, r, x
 
-    from . import services as services
+    from . import services
     from ._config import FlextApiConfig, config
     from ._settings import FlextApiSettings, settings
     from .api import FlextApi, api
