43cedc13ee3653adeb8310abe0d4e82bd7202382
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-23 14:18:56 -0300

    fixes

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,21 +9,9 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_cli import cli
-    from flext_web import web
+    from flext_web import cli, core, d, e, h, lazy_attribute, r, services, web, x
 
     from flext_api import api, c, config, m, main, p, s, settings, t, u
-    from flext_core import (
-        core,
-        d,
-        e,
-        h,
-        lazy,
-        lazy_attribute,
-        normalize_lazy_imports,
-        r,
-        x,
-    )
 
 
 __all__: tuple[str, ...] = (
@@ -35,14 +23,13 @@ __all__: tuple[str, ...] = (
     "d",
     "e",
     "h",
-    "lazy",
     "lazy_attribute",
     "m",
     "main",
-    "normalize_lazy_imports",
     "p",
     "r",
     "s",
+    "services",
     "settings",
     "t",
     "u",
@@ -65,19 +52,18 @@ _LAZY_IMPORTS = MappingProxyType(
                 "t",
                 "u",
             ),
-            "flext_cli": ("cli",),
-            "flext_core": (
+            "flext_web": (
+                "cli",
                 "core",
                 "d",
                 "e",
                 "h",
-                "lazy",
                 "lazy_attribute",
-                "normalize_lazy_imports",
                 "r",
+                "services",
+                "web",
                 "x",
             ),
-            "flext_web": ("web",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
